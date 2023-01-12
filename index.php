afdad2ebdc8d223a2835cc3fca869c9a0fa270f3
ander.uraga <ander.uraga@gmail.com>
2023-01-12 10:35:48 +0100

    cambio bobo

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php include 'navbar.php' ?>
 
   <div class="bg-light p-5 rounded">
-    <h1>Navbar example</h1>
+    <h1>Página Inicio</h1>
     <p class="lead">This example is a quick exercise to illustrate how fixed to top navbar works. As you scroll, it will remain fixed to the top of your browser’s viewport.</p>
     <a class="btn btn-lg btn-primary" href="/docs/5.3/components/navbar/" role="button">View navbar docs &raquo;</a>
   </div>
